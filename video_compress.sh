701388d1db8e2fb6233c6caf47f2b478aeb75f66
glathoud <glat@glat.info>
2022-11-11 12:57:13 +0300

    simple tool for video compression

---
+++ video_compress.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/bash
+
+# Video compression tool
+#
+# Based on
+# https://unix.stackexchange.com/questions/28803/how-can-i-reduce-a-videos-size-with-ffmpeg
+#
+# Examples:
+#
+# video_compress.sh input_file.mp4
+# video_compress.sh input_file.mp4 -half
+#
+# Both create a new file, which should be smaller than the original.
+# 
+# Guillaume Lathoud, 2022
+# The Boost License apply, see file ./LICENSE
+
+set -e
+
+ME="$(basename $0)"
+
+IN_FN="$1"
+HEAD="${IN_FN%.*}"
+TAIL="${IN_FN##*.}"
+
+if [[ ! -f "$IN_FN" ]]
+then
+    echo "$ME: could not find IN_FN: \"$IN_FN\""
+    exit 1
+fi
+
+opt=( -vcodec libx265 -crf 28 )
+if [[ "$2" == "-half" ]]
+then
+    opt=(  -vf "scale=trunc(iw/4)*2:trunc(ih/4)*2"  "${opt[@]}" )
+    HEAD="${HEAD}_half"
+fi
+
+HEAD="${HEAD}_x265"
+
+OUT_FN="${HEAD}.${TAIL}"
+
+if [[ -f "$OUT_FN" ]]
+then
+    echo "$ME: output already exists: OUT_FN:\"$OUT_FN\""
+    exit 2
+else
+    ffmpeg -i" $IN_FN" "${opt[@]}" "$OUT_FN"
+fi
