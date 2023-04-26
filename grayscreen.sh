f7b038e9cd21bf023192a45737c0ae6fc1a29bf0
glathoud <glat@glat.info>
2023-04-26 19:58:41 +0300

    example

--- grayscreen.sh
+++ grayscreen.sh
@@ -2,4 +2,9 @@
 
 # inspired from https://unix.stackexchange.com/questions/729772/xrandr-change-saturation-less-color-more-black-white
 
+if [ ! $1 ]; then
+    echo "example parameter: HDMI-1  or  eDP-1"
+    exit 1
+fi
+
 xrandr --output "$1" --set CTM '1431655765,0,1431655765,0,1431655765,0,1431655765,0,1431655765,0,1431655765,0,1431655765,0,1431655765,0,1431655765,0'
