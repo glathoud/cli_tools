c466a869af62224e6733067943f098c8fcbf46c7
glathoud <glat@glat.info>
2023-05-06 16:11:16 +0300

    default: do my screens: )

--- grayscreen.sh
+++ grayscreen.sh
@@ -4,7 +4,10 @@
 
 if [ ! $1 ]; then
     echo "example parameter: HDMI-1  or  eDP-1"
-    exit 1
+    echo "...which we do right away, by default"
+    "$0" "HDMI-1"
+    "$0" "eDP-1"
+    exit 0
 fi
 
 xrandr --output "$1" --set CTM '1431655765,0,1431655765,0,1431655765,0,1431655765,0,1431655765,0,1431655765,0,1431655765,0,1431655765,0,1431655765,0'
