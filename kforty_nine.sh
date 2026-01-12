afd3b67f92828a9847c363ae0bbed8fe1efd6d99
glathoud <guillaume.lathoud@outdooractive.com>
2026-01-12 10:29:13 +0300

    kforty_nine.sh

---
+++ kforty_nine.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+nmax=100
+while (( nmax-- )); do
+    CURRENT=`ps axo pid,command | grep -E '\bforty_nine\b'`
+    [ "$CURRENT" ] || break
+    
+    PID=`echo $CURRENT | cut -d ' ' -f 1`
+    echo "About to kill: ${PID}"
+    kill $PID
+
+    if [ "$PID_LIST" ]; then
+        PID_LIST="${PID_LIST},"
+    fi
+    PID_LIST="${PID_LIST}${PID}"
+done
+
+if [ "$PID_LIST" ]
+then
+    PID_LIST=" (${PID_LIST})"
+else
+    NOT="NOT "
+fi
+    
+MSG="forty_nine was ${NOT}running${PID_LIST}"
+if [ "`which zenity`" ]
+then
+    zenity --info --text="${MSG}"
+elif [ "`which xmessage`" ]
+then
+    xmessage "${MSG}"
+fi
+
+echo "${MSG}"
+
+if [ "$NOT" ]; then exit 1; fi
